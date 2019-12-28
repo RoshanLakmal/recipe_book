f569f948a0d82b93b1eeee1434554cfa2ad66584
Roshan Lakmal <roshanlakmal@Roshans-MacBook-Air.local>
2019-12-28 16:56:13 +1100

    created array and added static data and poupulated recipe list

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipe.model';
 
 @Component({
   selector: 'app-recipe-list',
@@ -7,6 +8,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RecipeListComponent implements OnInit {
 
+  recipes: Recipe[] = [ new Recipe('A Test Recipe', 'This is simply a test','https://www.justataste.com/wp-content/uploads/2018/01/easy-beef-and-broccoli-recipe.jpg')];
   constructor() { }
 
   ngOnInit() {
